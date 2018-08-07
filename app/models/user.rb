e17e755e045407b2ac9b9dbe2055ba50be2a5897
bjjeong <bjjeong@umich.edu>
2018-08-07 14:55:53 -0700

    Finish User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,50 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint(8)        not null, primary key
+#  email           :string           not null
+#  password_digest :string           not null
+#  session_token   :string           not null
+#  first_name      :string           not null
+#  last_name       :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+
 class User < ApplicationRecord
+    attr_reader :password
+
+    validates :email, :password_digest, :session_token, :first_name, :last_name, presence: true
+    validates :password, length: { minimum: 6, allow_nil: true }
+    validates :email, uniqueness: true
+    after_initialize :ensure_session_token
+    
+    def self.find_by_credentials(email, password)
+        user = User.find_by(username: username)
+		return nil unless user
+		user.password_is?(password) ? user : nil
+    end
+
+    def password=(value)
+        self.password_digest = BCrypt::Password.create(value)
+		@password = value
+    end
+
+    def is_password?(password)
+        BCrypt::Password.new(self.password_digest).is_password?(password)
+    end
+
+    def reset_session_token!
+        self.session_token = SecureRandom.urlsafe_base64
+        self.save!
+        self.session_token
+    end
+
+    private
+
+    def ensure_session_token
+        self.session_token ||= SecureRandom.urlsafe_base64
+    end
+    
 end
