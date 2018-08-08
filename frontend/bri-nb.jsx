93515bf16dc0b2fea63c7f46b56898dc4f9864c3
bjjeong <bjjeong@umich.edu>
2018-08-08 10:13:40 -0700

    Start adding React Router

--- frontend/bri-nb.jsx
+++ frontend/bri-nb.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import configureStore from './store/store';
 
 import * as ApiUtil from './util/session_api_util';
 
@@ -11,5 +12,9 @@ window.user2 = { user: { email: 'jeong@jeong.com', password: '123456', first_nam
 
 document.addEventListener('DOMContentLoaded', () => {
     const root = document.getElementById('root');
+    const store = configureStore();
+
+    window.store = configureStore();
+    
     ReactDOM.render(<h1>Welcome to Bri-nb</h1>, root);
 });
